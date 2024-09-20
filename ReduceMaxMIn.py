03c1a53df7b0f0883ef37e66cf0f0f6375dde1bc
FaithLoredoCelaya <63024446+FaithLoredoCelaya@users.noreply.github.com>
2024-09-19 21:33:53 -0400

    Added efficient code execution
    This code is intended to get 4 values from the user. find the largest number and the smallest number in the sequence and then add the two  of them together. this was all done with in 3 lines of code!

---
+++ ReduceMaxMIn.py
@@ -0,0 +1,4 @@
+def get_Min_Max(a,b,c,d):
+    print("\t",big:=max(a,b,c,d),"\n","\t",small:=min(a,b,c,d),"\n","\t\n","The sum of the minimum and maximum variables is",big+small)
+    
+get_Min_Max(int(input("Enter value of A:")),int(input("Enter value of B:")),int(input("Enter value of C:")),int(input("Enter value of D:")))
